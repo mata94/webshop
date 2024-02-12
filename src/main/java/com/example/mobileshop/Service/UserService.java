ce86f291ef857a133cb996518d25935268a7b1e2
franpavlovi <fljubuski@gmail.com>
2024-02-12 22:38:04 +0100

    user

---
+++ src/main/java/com/example/mobileshop/Service/UserService.java
@@ -0,0 +1,96 @@
+package com.example.mobileshop.Service;
+
+
+import com.example.mobileshop.models.Role;
+import com.example.mobileshop.models.User;
+import com.example.mobileshop.models.UserDetails;
+import com.example.mobileshop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public void checkEmailAndLoadUser(User user)
+    {
+        if(userRepository.findByEmail(user.getEmail()) != null){
+            throw new IllegalArgumentException("User vec postoji.");
+        }
+        else {
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            user.setPassword(encoder.encode(user.getPassword()));
+
+            Set<Role> roles = user.getRoles();
+            if (roles == null || roles.isEmpty()) {
+                roles = new HashSet<>();
+                roles.add(Role.USER);
+                user.setRoles(roles);
+            }
+
+            userRepository.save(user);
+        }
+    }
+    public void adminRegistration(){
+
+        User admin = new User();
+        admin.setFirstName("admin123");
+        admin.setLastName("admin123");
+        admin.setEmail("admin123@gmail.com");
+        admin.setPassword("admin123");
+
+        Set<Role> roles = new HashSet<>();
+        roles.add(Role.ADMIN);
+        admin.setRoles(roles);
+        User existingAdmin = userRepository.findByEmail("admin123@gmail.com");
+        if(existingAdmin == null){
+            this.checkEmailAndLoadUser(admin);
+        }else {
+            System.out.println("Admin already exists");
+        }
+
+    }
+
+    public Long getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+
+            if (principal instanceof UserDetails) {
+                UserDetails userDetails = (UserDetails) principal;
+
+                return userDetails.getUser().getId();
+            }
+        }
+
+        return null;
+    }
+
+    public User findCurrentUser()
+    {
+        if(this.getCurrentUserId() != null) {
+
+            Optional<User> user = userRepository.findById(this.getCurrentUserId());
+
+            if (user.isPresent()) {
+                return user.get();
+            } else {
+                throw new RuntimeException("User ne postoji");
+            }
+        }
+        throw new RuntimeException("Ne mozemo pronaci trenutnog usera");
+    }
+
+}
